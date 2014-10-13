e6457a12411c9279e5f1df432f83f76fb3f6cd30
Alex Beltyukov <beltyukov.1@gmail.com>
2014-10-13 12:36:15 -0500

    Add ability to add comments Fix comment upvoting

--- app.js
+++ app.js
@@ -53,4 +53,21 @@ angular.module('flapperNews', ['ui.router'])
   }])
   .controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if ($scope.body === '') {
+        return;
+      }
+
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+
+    $scope.incrementCommentUpvotes = function(comment){
+      comment.upvotes += 1;
+    }
   }]);
